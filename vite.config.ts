eca726b899f39db3dfd1f7aa4518223c1332a5d9
E Pabitra <epabitra8@gmail.com>
2025-02-08 11:39:06 +0530

    trying to deploy app

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/bullmantras",
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor'; // Moves node_modules into a separate chunk
+          }
+        }
+      }
+    },
+    chunkSizeWarningLimit: 1000, // Increase size limit (optional)
+  }
 })
